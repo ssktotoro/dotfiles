f1144691f258f7a6affe4a3c8400017cf5463152
Joshua Loyal <joshua.d.loyal@gmail.com>
2015-11-26 10:11:59 -0500

    add blue, nltk to bashrc

--- setup/bashrc.sh
+++ setup/bashrc.sh
@@ -27,6 +27,7 @@ fi
 
 # PATH
 PATH=$PATH:$HOME/bin:./
+PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export PATH
 
 # Text Editors
@@ -34,3 +35,24 @@ export SVN_EDITOR="vi"
 
 # Python
 #export PYTHONPATH=
+
+# virtualenvs
+export WORKON_HOME=$HOME/.virtualenv
+source /usr/local/bin/virtualenvwrapper.sh
+export PIP_REQUIRE_VIRTUALENV=true
+function gpip() {
+    PIP_REQUIRE_VIRTUALENV="" pip "$@"
+}
+
+# docker environemnt
+eval $(docker-machine env default)
+export DOCKON_HOME=$HOME/.dockerenv
+export PYTHONPATH=$HOME/myworkspace/dockerenv:$PYTHONPATH
+export PATH=$HOME/myworkspace/dockerenv/scripts:$PATH
+source $HOME/myworkspace/dockerenv/scripts/dockerenvwrapper
+
+# NLTK
+export NLTK_DATA=$HOME/nltk_data
+
+# BLUE
+export PYTHONPATH=$HOME/myworkspace/Blue:$PYTHONPATH
